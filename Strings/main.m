897bb05c1cb693352bd2e67520461440021152b8
SpencerSymington <theSpencerSymington@gmail.com>
2018-10-28 23:09:49 -0230

    Initial Commit

---
+++ Strings/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Strings
+//
+//  Created by Spencer Symington on 2018-10-28.
+//  Copyright © 2018 Spencer Symington. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
